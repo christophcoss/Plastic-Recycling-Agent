213b7ec9ca5259d1668aba2c00aee002b287b515
Chris <c.p.h.cosse@student.tudelft.nl>
2021-12-16 16:40:38 +0100

    Improved households

--- ProjectCode/Households.py
+++ ProjectCode/Households.py
@@ -1,14 +1,15 @@
 from mesa import Agent
 from enum import *
+from Waste import Waste
 
 class Households(Agent):
     def __init__(self, unique_id, model, type, infraAccess, recPerception, recImportance, recKnowledge, connected, proDecRate, recPlaCons, wasteProd):
         super().__init__(unique_id, model)
-        self.type = type
-        self.infraAccess = infraAccess
-        self.recPreception = recPerception
-        self.recImportance = recImportance
-        self.recKnowledge = recKnowledge
+        self.type = type #defines the ammount of trash produced
+        self.infraAccess = infraAccess #how difficult it is for them to recycle
+        self.recPerception = recPerception #how perceptive they are to changing their importance of recycling
+        self.recImportance = recImportance #how important it is for them to recycle
+        self.recKnowledge = recKnowledge #how good they are at recycling
         self.connected = connected
         self.prodDecRate = proDecRate
         self.recPlaCons = recPlaCons
@@ -26,8 +27,18 @@ class Households(Agent):
         return
     #TODO
 
+    def improveImportance(self, Activity):
+        if not self.connected:
+            return
+        newRecImp = (self.recImportance * (1 + Activity.efficiency))
+        self.recImportance = newRecImp if newRecImp < 1 else 1
 
 
+    def improveKnowledge(self, Activity):
+        if not self.connected:
+            return
+        newRecKnow = (self.recKnowledge * (1 + Activity.efficiency))
+        self.recImportance = newRecKnow if newRecKnow < 1 else 1
 
 class HouseholdType(Enum):
     INDIVIDUALS = "Individuals"
